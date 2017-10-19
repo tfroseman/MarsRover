afd3aa3e8d8835c50e6589ed9c8d242787f9bf32
Thomas Roseman <roseman.tom@gmail.com>
2017-10-19 13:40:57 -0400

    Added an extra argv
    Can now pass in a file path that points to a json file containing details about the map
    
    Signed-off-by: Thomas Roseman <roseman.tom@gmail.com>

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 /**
  * Develop an api that moves a rover around on a grid.
 
 • DONE You are given the initial starting point (x,y) of a rover and the direction (N,S,E,W) it is facing.
-• The rover receives a character array of commands.
-• Implement commands that move the rover forward/backward (f,b).
-• Implement commands that turn the rover left/right (l,r).
+• DONE The rover receives a character array of commands.
+• DONE Implement commands that move the rover forward/backward (f,b).
+• DONE Implement commands that turn the rover left/right (l,r).
 • Implement wrapping from one edge of the grid to another. (planets are spheres after all)
 • Implement obstacle detection before each move to a new square. If a given sequence of commands encounters an obstacle, 
 	the rover moves up to the last possible point and reports the obstacle.
@@ -13,22 +13,23 @@
 var MarsRover = require('./rover.js');
 
 
-function start(start_x, start_y, direction, commands) {
 
-	let rover = new MarsRover(start_x, start_y, direction);
-	console.log(`The rover has been created at ${rover.getX()}, ${rover.getY()} and is facing ${rover.getDirection()}`);
+function start(start_x, start_y, direction, commands, map_file) {
 
+	let rover = new MarsRover(start_x, start_y, direction, map_file);
+	
 	commands.split('').forEach((command) => {
 		switch (command) {
 			case 'l': rover.turnLeft(); break;
 			case 'r': rover.turnRight(); break;
 			case 'f': rover.moveForward(); break;
 			case 'b': rover.moveBackward(); break; 
+			default: console.error(`${command} is not supported`);
 		}
 	});
 
 }
 
 
-start(process.argv[2], process.argv[3], process.argv[4], process.argv[5]);
+start(process.argv[2], process.argv[3], process.argv[4], process.argv[5], process.argv[6]);
 
