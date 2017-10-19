87643b7c0ddf64f95cca0b06225f61ca7c4ab58d
Thomas Roseman <roseman.tom@gmail.com>
2017-10-19 15:11:09 -0400

    Cleaning up
    Signed-off-by: Thomas Roseman <roseman.tom@gmail.com>

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
 "use strict";
-/**
- * Develop an api that moves a rover around on a grid.
-
-• DONE You are given the initial starting point (x,y) of a rover and the direction (N,S,E,W) it is facing.
-• DONE The rover receives a character array of commands.
-• DONE Implement commands that move the rover forward/backward (f,b).
-• DONE Implement commands that turn the rover left/right (l,r).
-• Implement wrapping from one edge of the grid to another. (planets are spheres after all)
-• Implement obstacle detection before each move to a new square. If a given sequence of commands encounters an obstacle, 
-	the rover moves up to the last possible point and reports the obstacle.
- */
 var MarsRover = require('./rover.js');
-
-
 
 function start(start_x, start_y, direction, commands, map_file) {
 
@@ -32,4 +19,3 @@ function start(start_x, start_y, direction, commands, map_file) {
 
 
 start(process.argv[2], process.argv[3], process.argv[4], process.argv[5], process.argv[6]);
-
